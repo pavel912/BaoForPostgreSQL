f9732848fc30ca86f7edf131e7680d1688a33ceb
pavel.lobanov <pavel.lobanov@uni-konstanz.de>
2024-12-05 20:15:55 +0100

    change run_queries to run

--- run_queries_pg.py
+++ run_queries_pg.py
@@ -4,7 +4,7 @@ import sys
 import random
 from time import time, sleep
 
-USE_BAO = True
+USE_BAO = False
 PG_CONNECTION_STR = "dbname=imdb user=imdb host=localhost"
 
 # https://stackoverflow.com/questions/312443/
@@ -20,10 +20,11 @@ def run_query(sql, bao_select=False, bao_reward=False):
         try:
             conn = psycopg2.connect(PG_CONNECTION_STR)
             cur = conn.cursor()
-            cur.execute(f"SET enable_bao TO {bao_select or bao_reward}")
-            cur.execute(f"SET enable_bao_selection TO {bao_select}")
-            cur.execute(f"SET enable_bao_rewards TO {bao_reward}")
-            cur.execute("SET bao_num_arms TO 5")
+            cur.execute("SET pg_bao.bao_host TO localhost")
+            cur.execute(f"SET pg_bao.enable_bao TO {bao_select or bao_reward}")
+            cur.execute(f"SET pg_bao.enable_bao_selection TO {bao_select}")
+            cur.execute(f"SET pg_bao.enable_bao_rewards TO {bao_reward}")
+            cur.execute("SET pg_bao.bao_num_arms TO 5")
             cur.execute("SET statement_timeout TO 300000")
             cur.execute(q)
             cur.fetchall()
