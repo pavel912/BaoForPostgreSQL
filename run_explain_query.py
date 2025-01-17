b6b515628d254bd72a1a395a36ef7671f6a64257
Pavel Lobanov <pavel.lobanov@uni-konstanz.de>
2025-01-18 00:47:24 +0100

    enhance explain

--- run_explain_query.py
+++ run_explain_query.py
@@ -8,7 +8,7 @@ from time import time, sleep
 PG_CONNECTION_STR = "dbname=imdb user=imdb host=localhost"
 
 
-def exaplain_query(sql):
+def explain_query(sql):
     conn = psycopg2.connect(PG_CONNECTION_STR)
     cur = conn.cursor()
     cur.execute("SET pg_bao.bao_host TO localhost")
@@ -17,17 +17,28 @@ def exaplain_query(sql):
     cur.execute(f"SET pg_bao.enable_bao_rewards TO {False}")
     cur.execute("SET pg_bao.bao_num_arms TO 5")
     cur.execute("SET statement_timeout TO 300000")
-    cur.execute("EXPLAIN " + sql)
+    cur.execute("EXPLAIN (ANALYZE, COSTS, VERBOSE, BUFFERS, FORMAT JSON) " + sql)
     query_plan = cur.fetchall()
     conn.close()
     return query_plan
 
-query_path = sys.argv[1]
+query_paths = sys.argv[1:]
+queries = []
+for fp in query_paths:
+    with open(fp) as f:
+        query = f.read()
+    queries.append((fp, query))
 
-with open(query_path) as f:
-    query = f.read()
+print("Explaining queries")
 
-print("Explaining query", query)
+data = []
 
-for line in exaplain_query(query):
-    print(line[0])
+for query in queries:
+    data.append([query[0], query[1], explain_query(query[1])])
+
+with open("query_plans") as f:
+    for d in data:
+        f.write("Query path: " + d[0] + "\n")
+        f.write(d[1] + "\n")
+        f.write(d[2][0][0] + "\n")
+        f.write("\n")
